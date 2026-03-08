f561f5d0a5388ab63b1f8b5b1b0d0fe73eab5c98
lehuyenthienphuc3594-afk <lehuyenthienphuc3594@gmail.com>
2026-03-08 16:48:19 +0700

    Add letters and functions for greeting messages

---
+++ script.js
@@ -0,0 +1,141 @@
+const letters = {
+
+    
+"Võ Minh An":
+"Chúc bạn có 1 ngày 8/3 vui vẻ, tôi biết bạn đã có 1 kết quả thi học sinh giỏi không như mong muốn, nhưng hãy nhớ rằng điều đáng tự hào không phải việc bạn chưa từng thất bại, mà là bạn vẫn đứng dậy sau những lần thất bại. Chúc bạn có thể đạt được thành tích mình mong muốn trong tương lai",
+
+"Nguyễn Ngọc Phương Anh":
+"Chúc bạn 8/3 hạnh phúc, chúc bạn sẽ học giỏi, xinh đẹp, thành công trong học tập và nhiều việc kháng. Mong bạn sẽ luôn vui tươi và hạnh phúc",
+
+"Nguyễn Ngọc Bích":
+"8/3 vui vẻ, không làm bạn với tôi cũng được. chúc bạn có 1 ngày vui vẻ, thành công trong tương lai",
+
+"Nguyễn Phạm Bảo Ngân":
+"Chúc bạn có 1 ngày vui vẻ, chúc cho bạn luôn học giỏi, thành công trong học tập, ngày càng xinh đẹp và luôn luôn hạnh phúc. chúc bạn năm sau đạt giải học sinh giỏi môn toán nhé",
+
+"Nguyễn Thị Phương Uyên":
+"Chúc bạn có ngày 8/3 vui vẻ, tôi biết bạn đã có một kết quả thi học sinh giỏi không được như mong muốn, nhưng đừng buồn vì việc bạn được đi thi vượt cấp đã rất tuyệt vời rồi. Và hơn hết, điều đáng tự hào không phải bạn chưa bao giờ vấp ngã, mà điều đáng tự hào chính là bạn luôn đứng dậy sau mỗi lần vấp ngã. chúc cho bạn luôn vui vẻ, xinh đẹp và đạt nhiều thành công trong tương lai",
+
+"Nguyễn Đỗ Bảo Ngọc":
+"Chúc bạn có ngày 8/3 hạnh phúc, chúc mừng bạn đã có một kết quả tốt trong kỳ thi học sinh giỏi cấp phường vừa rồi, mong bạn sẽ đạt được thành tích như mong muốn trong kỳ thi cấp tỉnh. chúc bạn luôn hạnh phúc, xinh đẹp, tràn đầy năng lượng và đạt nhiều thành công trong tương lai",
+
+"Vũ Trần Nam Phương":
+"Chúc bạn có 1 ngày 8/3 vui vẻ, chúc bạn luôn xinh đẹp, học giỏi và đạt được nhiều thành công trong tương lai. chúc bạn đạt được thành tích mong muốn trong kỳ thi giữa học kỳ 2 sắp tới",
+
+"Vũ Trần Ngọc Khánh":
+"Xin lỗi nếu mình đã không thể cho bạn xem được trong ngày 8/3. chúc bạn luôn hạnh phúc, vui vẻ, học giỏi và đạt được nhiều thành công",
+
+"Phan Bảo Châu":
+"Chúc bạn có 1 ngày 8/3 vui vẻ, sắp tới là thi giữa học kỳ 2, chúc bạn ôn luyện tốt và có đạt được số điểm mình mong muốn trong kỳ thi sắp tới. Chân thành cảm ơn bạn vì đã chúc mình thi tốt vào ngày 26/2 nhé!",
+
+"Phạm Lê Minh Châu":
+"Chúc bạn 8/3 vui vẻ. chúc bạn xinh đẹp, hạnh phúc, đạt nhiều thành công trong học tập. chúc bạn có 1 kết quả thi giữa học kỳ 2 như mong muốn",
+
+"Dương Bình Khánh Thư":
+"Chúc bạn 8/3 vui vẻ, học giỏi và ngày càng xinh đẹp. chúc bạn có 1 kết quả thi giữa kỳ 2 như mong muốn, mong bạn luôn hạnh phúc và đạt nhiều thành công",
+
+"Phan Thanh Nhi":
+"Chúc bạn có 1 ngày 8/3 vui vẻ, chúc bạn luôn hạnh phúc, học giỏi, xinh đẹp và đạt được nhiều thành công",
+
+"Nay Vân Nhi":
+"Chúc bạn 8/3 vui vẻ, chúc bạn ngày càng hạnh phúc, xinh đẹp, học giỏi và thành công nhé!. Và cả trong đường tình duyên nữa",
+
+"Trương Nguyên Thiên Ý":
+"Chúc bạn 8/3 hạnh phúc, chúc bạn học giỏi, xinh đẹp, luôn luôn cố gắng trong học tập và đạt được nhiều thành quả như kỳ vọng nhé. Sắp tới là thi giữa học kỳ 2 rồi, chúc bạn thành công trong ôn tập và thi cử",
+
+"Nguyễn Thục Quyên":
+"Chúc bạn có 1 ngày 8/3 vui vẻ, chúc bạn ngày càng học giỏi, xinh đẹp và đạt được nhiều thành công. chúc bạn có 1 kỳ ôn tập và thi cử thành công",
+
+"Nguyễn Trúc Ly":
+"Chúc bạn 8/3 hạnh phúc, chúc bạn học giỏi, xinh đẹp, và đạt nhiều thành công",
+
+"Cá Bé":
+"Chúc bro có 1 ngày 8/3 hạnh phúc. Chúc bro luôn vui vẻ, hạnh phúc, xinh đẹp và đạt được nhiều thành công nhé. Cảm ơn vì đã luôn làm bạn với tui",
+
+"Liêu Ngọc Khanh":
+"Chúc bro có 1 ngày 8/3 hạnh phúc. Chúc bro luôn vui vẻ, hạnh phúc, xinh đẹp và đạt được nhiều thành công nhé. Cảm ơn vì đã luôn làm bạn với tui",
+
+"Trần Châu Bảo Hân":
+"Chúc em gái yêu dấu của anh có một ngày 8/3 hạnh phúc, chúc em luôn luôn xinh đẹp, ngày càng học giỏi và đạt được nhiều thành công. sắp tới là thi giữa học kỳ 2 rồi, tuy lúc đó anh có thể sẽ không đồng hành cùng em được, nhưng chúc em ôn thi tốt và có 1 kết quả như mong muốn nhé",
+
+"Nguyễn Minh Trí":
+"Ủa Trí??? ,tôi biết cậu sẽ vào đây mà, nhưng thôi thì chúc thi giữa kỳ 2 có thành tích như mong muốn nhé"
+
+};
+
+function checkName(){
+
+let name=document.getElementById("name").value.trim();
+
+let envelope=document.getElementById("envelope");
+let title=document.getElementById("title");
+let letter=document.getElementById("letter");
+
+if(letters[name]){
+
+envelope.style.display="block";
+
+title.innerText="Gửi "+name;
+letter.innerText=letters[name];
+
+firework();
+
+}else{
+
+envelope.style.display="block";
+
+title.innerText="";
+letter.innerText="❌ Không tìm thấy tên. Hãy nhập đúng họ và tên.";
+
+}
+
+}
+
+function flower(){
+
+let f=document.createElement("div");
+
+f.classList.add("flower");
+
+f.innerHTML="🌸";
+
+f.style.left=Math.random()*100+"vw";
+
+f.style.animationDuration=3+Math.random()*4+"s";
+
+document.body.appendChild(f);
+
+setTimeout(()=>{
+
+f.remove();
+
+},7000);
+
+}
+
+setInterval(flower,300);
+
+function firework(){
+
+for(let i=0;i<20;i++){
+
+let fw=document.createElement("div");
+
+fw.classList.add("firework");
+
+fw.innerHTML="✨";
+
+fw.style.left=Math.random()*100+"vw";
+fw.style.top=Math.random()*100+"vh";
+
+document.body.appendChild(fw);
+
+setTimeout(()=>{
+
+fw.remove();
+
+},1000);
+
+}
+
+}
